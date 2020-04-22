0a136898286a8431816c95ae13ab6e3b0f3dcfb7
Xin Zhe Khooi <khooi8913@gmail.com>
2020-04-22 02:58:05 +0000

    init

---
+++ src/controller.py
@@ -0,0 +1,115 @@
+import argparse
+import grpc
+import os
+import sys
+from time import sleep
+
+# Import P4Runtime lib from parent utils dir
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 '../utils/'))
+import p4runtime_lib.bmv2
+from p4runtime_lib.switch import ShutdownAllSwitchConnections
+import p4runtime_lib.helper
+
+def writeFlowRules(p4info_helper, sw):
+    # TODO: Define your flow rules here
+
+    # table_entry = p4info_helper.buildTableEntry(
+    #     table_name="MyEgress.swtrace",
+    #     action_name="MyEgress.add_swtrace",
+    #     action_params={
+    #         "swid": sw.device_id,
+    #     })
+    # sw.WriteTableEntry(table_entry)
+    print "Installed ? flow rule on %s" % sw.name
+
+
+def readTableRules(p4info_helper, sw):
+    """
+    Reads the table entries from all tables on the switch.
+
+    :param p4info_helper: the P4Info helper
+    :param sw: the switch connection
+    """
+    print '\n----- Reading tables rules for %s -----' % sw.name
+    for response in sw.ReadTableEntries():
+        for entity in response.entities:
+            entry = entity.table_entry
+            table_name = p4info_helper.get_tables_name(entry.table_id)
+            print '%s: ' % table_name,
+            for m in entry.match:
+                print p4info_helper.get_match_field_name(table_name, m.field_id),
+                print '%r' % (p4info_helper.get_match_field_value(m),),
+            action = entry.action.action
+            action_name = p4info_helper.get_actions_name(action.action_id)
+            print '->', action_name,
+            for p in action.params:
+                print p4info_helper.get_action_param_name(action_name, p.param_id),
+                print '%r' % p.value,
+            print
+            
+
+def printGrpcError(e):
+    print "gRPC Error:", e.details(),
+    status_code = e.code()
+    print "(%s)" % status_code.name,
+    traceback = sys.exc_info()[2]
+    print "[%s:%d]" % (traceback.tb_frame.f_code.co_filename, traceback.tb_lineno)
+
+def main(p4info_file_path, bmv2_file_path):
+    # Instantiate a P4Runtime helper from the p4info file
+    p4info_helper = p4runtime_lib.helper.P4InfoHelper(p4info_file_path)
+
+    try:
+        # Create a switch connection object for s1 and s2;
+        # this is backed by a P4Runtime gRPC connection.
+        # Also, dump all P4Runtime messages sent to switch to given txt files.
+        s1 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
+            name='s1',
+            address='127.0.0.1:50051',
+            device_id=0,
+            proto_dump_file='logs/s1-p4runtime-requests.txt')
+
+        # Send master arbitration update message to establish this controller as
+        # master (required by P4Runtime before performing any other write operation)
+        s1.MasterArbitrationUpdate()
+
+        # Install the P4 program on the switches
+        s1.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
+                                       bmv2_json_file_path=bmv2_file_path)
+        print "Installed P4 Program using SetForwardingPipelineConfig on s1"
+       
+        # Write rules to the switches
+        for s in [s1]:
+            writeFlowRules(p4info_helper, sw=s)
+        
+        readTableRules(p4info_helper, s1)
+
+    except KeyboardInterrupt:
+        print " Shutting down."
+    except grpc.RpcError as e:
+        printGrpcError(e)
+
+    ShutdownAllSwitchConnections()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='P4Runtime Controller')
+    parser.add_argument('--p4info', help='p4info proto in text format from p4c',
+                        type=str, action="store", required=False,
+                        default='./build/basic.p4.p4info.txt')
+    parser.add_argument('--bmv2-json', help='BMv2 JSON file from p4c',
+                        type=str, action="store", required=False,
+                        default='./build/basic.json')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.p4info):
+        parser.print_help()
+        print "\np4info file not found: %s\nHave you run 'make'?" % args.p4info
+        parser.exit(1)
+    if not os.path.exists(args.bmv2_json):
+        parser.print_help()
+        print "\nBMv2 JSON file not found: %s\nHave you run 'make'?" % args.bmv2_json
+        parser.exit(1)
+    main(args.p4info, args.bmv2_json)
+
